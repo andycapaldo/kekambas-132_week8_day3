2a0f9ac1e2c81e30bd91c090b1af36f046fd57cf
Andrew Capaldo <andycapaldo2@gmail.com>
2023-11-08 17:11:01 -0700

    Finished Project

---
+++ dist/index.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
+function createUser(name, age) {
+    const newUser = {
+        id: (0, uuid_1.v4)(),
+        name,
+        age,
+        cart: []
+    };
+    return newUser;
+}
+function createItem(name, price, description) {
+    const newItem = {
+        id: (0, uuid_1.v4)(),
+        name,
+        price,
+        description
+    };
+    return newItem;
+}
+function addToCart(user, item) {
+    user.cart.push(item);
+}
+function removeFromCart(user, itemToRemove) {
+    user.cart = user.cart.filter(item => item.id !== itemToRemove.id);
+}
+function removeQuantityFromCart(user, itemToRemove, quantity) {
+    let x = 0;
+    while (x > quantity) {
+        user.cart.splice(user.cart.findIndex((i) => i.id == itemToRemove.id), 1);
+        x++;
+    }
+}
+function cartTotal(user) {
+    let total = 0;
+    for (let x of user.cart) {
+        total += x.price;
+    }
+    console.log(total);
+}
+function printCart(user) {
+    for (let x of user.cart) {
+        console.log(x.name);
+    }
+}
+const andy = createUser('Andy', 27);
+const hotTopicShirt = createItem('Flaming Tee', 49.99, 'Flaming Hot Topic T-Shirt');
+const bananas = createItem('Bananas', 2.99, '1 lb of Yellow Chiquita Bananas');
+const gucciBandanna = createItem('Gucci Bandanna', 189.99, 'Gucci Bandanna Gucci Gucci Bandanna');
+addToCart(andy, hotTopicShirt);
+printCart(andy);
+cartTotal(andy);
+addToCart(andy, bananas);
+addToCart(andy, bananas);
+addToCart(andy, bananas);
+printCart(andy);
+cartTotal(andy);
+addToCart(andy, gucciBandanna);
+addToCart(andy, gucciBandanna);
+addToCart(andy, gucciBandanna);
+printCart(andy);
+cartTotal(andy);
+removeFromCart(andy, bananas);
+printCart(andy);
+cartTotal(andy);
+removeQuantityFromCart(andy, gucciBandanna, 2);
+printCart(andy);
+cartTotal(andy);
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
+
+type Item = {
+    readonly id: string,
+    name: string,
+    price: number,
+    description: string
+};
+
+type User = {
+    readonly id: string,
+    name: string,
+    age: number,
+    cart: Item[]
+}
+
+function createUser(name:string, age:number){ 
+    const newUser: User = {
+        id: uuidv4(),
+        name,
+        age,
+        cart: []
+    }
+    return newUser;
+}
+
+function createItem(name:string, price:number, description:string){
+    const newItem: Item = {
+        id: uuidv4(),
+        name,
+        price,
+        description
+    };
+    return newItem;
+}
+
+function addToCart(user:User, item: Item){
+    user.cart.push(item)
+}
+
+function removeFromCart(user: User, itemToRemove: Item){
+    user.cart = user.cart.filter(item => item.id !== itemToRemove.id)
+}
+
+function removeQuantityFromCart(user: User, itemToRemove: Item, quantity:number){
+    let x = 0
+    while (x > quantity){
+        user.cart.splice(user.cart.findIndex((i) => i.id == itemToRemove.id), 1)
+        x++
+    }
+}
+
+function cartTotal(user:User){
+    let total = 0
+    for (let x of user.cart){
+        total+= x.price
+    }
+    console.log(total) 
+}
+
+function printCart(user:User){
+    for(let x of user.cart){
+        console.log(x.name)
+    }
+}
+
+
+const andy = createUser('Andy', 27);
+const hotTopicShirt = createItem('Flaming Tee', 49.99, 'Flaming Hot Topic T-Shirt');
+const bananas = createItem('Bananas', 2.99, '1 lb of Yellow Chiquita Bananas');
+const gucciBandanna = createItem('Gucci Bandanna', 189.99, 'Gucci Bandanna Gucci Gucci Bandanna')
+
+addToCart(andy, hotTopicShirt);
+printCart(andy);
+cartTotal(andy);
+
+addToCart(andy, bananas);
+addToCart(andy, bananas);
+addToCart(andy, bananas);
+printCart(andy);
+cartTotal(andy);
+
+addToCart(andy, gucciBandanna);
+addToCart(andy, gucciBandanna);
+addToCart(andy, gucciBandanna);
+printCart(andy);
+cartTotal(andy);
+
+removeFromCart(andy, bananas);
+printCart(andy);
+cartTotal(andy);
+
+removeQuantityFromCart(andy, gucciBandanna, 2);
+printCart(andy);
+cartTotal(andy);
